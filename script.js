16eea2b11b3928e0f56d2c83fb75ca7cc7caa38b
volkycode <volkycode@gmail.com>
2023-11-09 21:57:52 -0600

    Rock paper scissors v2
    Added a basic UI using the DOM
    Added event listeners

--- script.js
+++ script.js
@@ -2,14 +2,10 @@ let userChoice;
 let computerChoice;
 let userScore = 0;
 let computerScore = 0;
-let loop = true;
-function getPlayerChoice(){
-    let choice  = prompt('choose either rock paper or scissors');
-    while (choice !== 'rock' && choice !== 'paper' && choice !== 'scissors') {
-        choice = prompt('You didn\'t choose rock, paper, or scissors. Please choose again:').toLowerCase();
-    }
-    return choice;
-}
+const res = document.querySelector('div');
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
 
 function getComputerChoice(){
     let rand = Math.round(Math.random()*2);
@@ -26,38 +22,58 @@ function getComputerChoice(){
             break;
     } 
 }
-function match(){
+function match(userChoice, computerChoice){
+    computerChoice = getComputerChoice();
     if(userChoice === computerChoice){
-        console.log('its a tie');
+        res.innerHTML = (`you: ${userScore} - computer: ${computerScore}, it\'s a tie`);
     } else if(
         (userChoice === 'rock' && computerChoice === 'scissors') || 
         (userChoice === 'scissors' && computerChoice === 'paper') ||
         (userChoice === 'paper' && computerChoice === 'rock')
     ){
-        console.log('you win');
-        userScore += 1;
+        if(userScore == 4){
+            res.innerHTML = ('you\'ve won');
+            rock.removeEventListener('click', handleClickRock);
+            paper.removeEventListener('click', handleClickPaper);
+            scissors.removeEventListener('click', handleClickScissors);
+        } else{
+            res.innerHTML = (`you: ${userScore} - computer: ${computerScore}`);
+            userScore += 1;
+        }
+        console.log(userScore);
     } else{
-        console.log('you lose');
-        computerScore += 1;
+        if(computerScore == 4){
+            res.innerHTML = ('you\'ve lost');
+            rock.removeEventListener('click', handleClickRock);
+            paper.removeEventListener('click', handleClickPaper);
+            scissors.removeEventListener('click', handleClickScissors);
+        } else{
+            res.innerHTML = (`you: ${userScore} - computer: ${computerScore}`);
+            computerScore += 1;
+        }
+       
     }
+} 
+function handleClickRock() {
+    userChoice = 'rock';
+    computerChoice = getComputerChoice();
+    match(userChoice, computerChoice);
 }
-function game(loop){
-    while(loop == true){
-        userChoice = getPlayerChoice();
-        console.log(userChoice);
-        computerChoice = getComputerChoice();
-        console.log(computerChoice);
-        match();
-        if(computerScore === 5){
-            console.log('you\'ve lost');
-            loop = false;
-        }
-        else if(userScore === 5){
-            console.log('you\'ve won');
-            loop = false;
-        }
-        
-    }
+
+function handleClickPaper() {
+    userChoice = 'paper';
+    computerChoice = getComputerChoice();
+    match(userChoice, computerChoice);
+}
+
+function handleClickScissors() {
+    userChoice = 'scissors';
+    computerChoice = getComputerChoice();
+    match(userChoice, computerChoice);
+}
+
+rock.addEventListener('click', handleClickRock);
+paper.addEventListener('click', handleClickPaper);
+scissors.addEventListener('click', handleClickScissors);
+
  
-}
-game(loop);
